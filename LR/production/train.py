c83f7767aa56b197163fafbf772957dacfcb876d
octopus <345293721@qq.com>
2019-12-30 11:44:59 +0800

    Update train.py

--- LR/production/train.py
+++ LR/production/train.py
@@ -26,17 +26,13 @@ def train_lr_model(train_file, model_coef, model_file, feature_num_file):
     train_feature = np.genfromtxt(train_file, dtype=np.int32, delimiter=",", usecols=feature_list)
     lr_cf = lrcv(Cs=[1], penalty="l2", tol=0.0001, max_iter=500, cv=5).fit(train_feature, train_label)
     scores = lr_cf.scores_.values()[0]
-    print
-    "diff:%s" % (",".join([str(ele) for ele in scores.mean(axis=0)]))
-    print
-    "Accuracy:%s (+-%0.2f)" % (scores.mean(), scores.std() * 2)
+    print("diff:%s" % (",".join([str(ele) for ele in scores.mean(axis=0)])))
+    print("Accuracy:%s (+-%0.2f)" % (scores.mean(), scores.std() * 2))
     lr_cf = lrcv(Cs=[1], penalty="l2", tol=0.0001, max_iter=500, cv=5, scoring="roc_auc").fit(train_feature,
                                                                                               train_label)
     scores = lr_cf.scores_.values()[0]
-    print
-    "diff:%s" % (",".join([str(ele) for ele in scores.mean(axis=0)]))
-    print
-    "AUC:%s (+-%0.2f)" % (scores.mean(), scores.std() * 2)
+    print("diff:%s" % (",".join([str(ele) for ele in scores.mean(axis=0)])))
+    print("AUC:%s (+-%0.2f)" % (scores.mean(), scores.std() * 2))
     coef = lr_cf.coef_[0]
     fw = open(model_coef, "w+")
     fw.write(",".join(str(ele) for ele in coef))
@@ -46,8 +42,7 @@ def train_lr_model(train_file, model_coef, model_file, feature_num_file):
 
 if __name__ == "__main__":
     if len(sys.argv) < 5:
-        print
-        "usage: python xx.py train_file coef_file model_file featuren_num_file"
+        print("usage: python xx.py train_file coef_file model_file featuren_num_file")
         sys.exit()
     else:
         train_file = sys.argv[1]
