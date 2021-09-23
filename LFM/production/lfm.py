31a93e0b79fd5c3745c2099f468693125ce4db58
张瑀 <zhangyu_xtb@geekplus.cc>
2021-09-23 16:29:05 +0800

    master:推荐项目中增加数据

--- LFM/production/lfm.py
+++ LFM/production/lfm.py
@@ -1,9 +1,10 @@
 # -*-coding:utf8-*-
 """
 author:zhangyu
-训练LFM模型
 email:zhangyuyu417@gmail.com
+description:训练LFM模型
 """
+from typing import List, Tuple, Dict
 
 import numpy as np
 import sys
@@ -11,9 +12,13 @@ import sys
 sys.path.append("../util")
 import LFM.util.read as read
 import operator
+import time
+
+user_file_path = "../data/ratings.txt"
+item_file_path = "../data/movies.txt"
 
 
-def lfm_train(train_data, F, alpha, beta, step):
+def lfm_train(train_data: List[List[object]], F: int, alpha: float, beta: float, step: int) -> Tuple[Dict, Dict]:
     """
     Args:
         train_data: 训练LFM模型的数据
@@ -22,27 +27,27 @@ def lfm_train(train_data, F, alpha, beta, step):
         beta: 学习率
         step: 迭代数
     Return:
-        dict: key itemid, value:np.ndarray
-        dict: key userid, value:np.ndarray
+        dict: key item_id, value:np.ndarray
+        dict: key user_id, value:np.ndarray
     """
     user_vec = {}
     item_vec = {}
     for step_index in range(step):
         for data_instance in train_data:
-            userid, itemid, label = data_instance
-            if userid not in user_vec:
-                user_vec[userid] = init_model(F)
-            if itemid not in item_vec:
-                item_vec[itemid] = init_model(F)
-            delta = label - model_predict(user_vec[userid], item_vec[itemid]) 
+            user_id, item_id, label = data_instance
+            if user_id not in user_vec:
+                user_vec[user_id] = init_model(F)
+            if item_id not in item_vec:
+                item_vec[item_id] = init_model(F)
+            delta = label - model_predict(user_vec[user_id], item_vec[item_id])
             for index in range(F):
-                user_vec[userid][index] += beta * (delta * item_vec[itemid][index] - alpha * user_vec[userid][index])
-                item_vec[itemid][index] += beta * (delta * user_vec[userid][index] - alpha * item_vec[itemid][index])
+                user_vec[user_id][index] += beta * (delta * item_vec[item_id][index] - alpha * user_vec[user_id][index])
+                item_vec[item_id][index] += beta * (delta * user_vec[user_id][index] - alpha * item_vec[item_id][index])
         beta = beta * 0.9
     return user_vec, item_vec
 
 
-def init_model(vector_len):
+def init_model(vector_len: int):
     """
     Args:
         vector_len: 向量长度
@@ -65,61 +70,76 @@ def model_predict(user_vector, item_vector):
     return res
 
 
-def model_train_process():
+def model_train_process() -> List[List[str]]:
     """
-        测试lfm模型
+        latent factor model模型
     """
-    train_data = read.get_train_data("../data/ratings.txt")
+    train_data = read.get_train_data(user_file_path)
     user_vec, item_vec = lfm_train(train_data, 50, 0.01, 0.1, 50)
-    for userid in user_vec:
-        recom_result = give_recom_result(user_vec, item_vec, userid)
-        # ana_recom_result(train_data, userid, recom_result)
+    res = {}
+    for user_id in user_vec:
+        recom_result = give_recom_result(user_vec, item_vec, user_id)
+        temp_res = ana_recom_result(train_data, user_id, recom_result)
+        res[user_id] = temp_res
+    return res
 
 
-def give_recom_result(user_vec, item_vec, userid):
+def give_recom_result(user_vec: List[str], item_vec: List[str], user_id: str) -> List[Tuple]:
     """
         用LFM模型获取固定结果
     Args:
         user_vec: LFM模型结果
         item_vec: LFM模型结果
-        userid: 固定用户id
+        user_id: 固定用户id
     Return:
-        list:[(itemid, score), (itemid1, score1)]
+        list:[(item_id, score), (item_id1, score1)]
     """
     fix_num = 10
-    if userid not in user_vec:
+    if user_id not in user_vec:
         return []
     record = {}
     recom_list = []
-    user_vector = user_vec[userid]
-    for itemid in item_vec:
-        item_vector = item_vec[itemid]
+    user_vector = user_vec[user_id]
+    for item_id in item_vec:
+        item_vector = item_vec[item_id]
         res = np.dot(user_vector, item_vector) / (np.linalg.norm(user_vector) * np.linalg.norm(item_vector))
-        record[itemid] = res
-    for zuhe in sorted(record.iteritems(), key=operator.itemgetter(1), reverse=True)[:fix_num]:
-        itemid = zuhe[0]
-        score = round(zuhe[1], 3)
-        recom_list.append((itemid, score))
+        record[item_id] = res
+    for item in sorted(record.items(), key=operator.itemgetter(1), reverse=True)[:fix_num]:
+        item_id = item[0]
+        score = round(item[1], 3)
+        recom_list.append((item_id, score))
     return recom_list
 
 
-def ana_recom_result(train_data, userid, recom_list):
+def ana_recom_result(train_data, user_id: str, recom_list) -> List[List[str]]:
     """
         测试结果
     Args:
         train_data: 测试模型
-        userid:固定用户id
+        user_id:固定用户id
         recom_list: 模型推荐结果
+    Return:
+        结果List
     """
-    item_info = read.get_item_info("../data/movies.txt")
-    for data_instance in train_data:
-        tmp_userid, itemid, label = data_instance
-        if tmp_userid == userid and label == 1:
-            print(item_info[itemid])
-    print("recom result")
-    for zuhe in recom_list:
-        print(item_info[zuhe[0]])
+    result = []
+    item_info = read.get_item_info(item_file_path)
+    # for data_instance in train_data:
+    #     tmp_user_id, item_id, label = data_instance
+    #     if tmp_user_id == user_id and label == 1:
+    #         pass
+    #         # print(item_info[item_id])
+    #         # result.append(item_info[item_id])
+    # print("recom result")
+    for recom in recom_list:
+        item_id = recom[0]
+        result.append(item_info[item_id][0])
+    return result
 
 
 if __name__ == "__main__":
-    model_train_process()
+    start_time = time.time()
+    res = model_train_process()
+    for r in res:
+        print(r, res[r][-3:])
+    end_time = time.time()
+    print(f"程序总运行时间: {end_time - start_time} s")
--- LFM/util/read.py
+++ LFM/util/read.py
@@ -1,81 +1,83 @@
-#-*-coding:utf8-*-
 """
 工具类
 author:zhangyu
 email:zhangyuyu417@gmail.com
-
+description:处理统计数据
 """
 
 import os
+from typing import List, Dict
+import json
 
 
-def get_item_info(input_file):
+def get_item_info(input_file: str) -> Dict:
     """
         获取商品信息
     Args:
-        输入文件
+        input_file:输入文件
     Return:
        商品信息字典
     """
     if not os.path.exists(input_file):
         return {}
     item_info = {}
-    linenum = 0
+    line_num = 0
     fp = open(input_file)
     for line in fp:
-        if linenum == 0:
-            linenum += 1
+        if line_num == 0:
+            line_num += 1
             continue
         item = line.strip().split(',')
         if len(item) < 3:
             continue
         elif len(item) == 3:
-            itemid, title, genre = item[0], item[1], item[2]
+            item_id, title, genre = item[0], item[1], item[2]
         elif len(item) > 3:
-            itemid = item[0]
+            item_id = item[0]
             genre = item[-1]
             title = ",".join(item[1:-1])
-        item_info[itemid] = [title, genre]
+        item_info[item_id] = [title, genre]
     fp.close()
     return item_info
 
 
-def get_ave_score(input_file):
+def get_ave_score(input_file: str) -> Dict:
     """
         获取点击率分数
     Args:
-        输入文件
+        input_file:输入文件
     Return:
-        商品id和字典id
+        商品id和点击次数Dict
     """
     if not os.path.exists(input_file):
         return {}
-    linenum = 0
+    line_num = 0
     record_dict = {}
     score_dict = {}
     fp = open(input_file)
     for line in fp:
-        if linenum == 0:
-            linenum += 1
+        if line_num == 0:
+            line_num += 1
             continue
         item = line.strip().split(',')
         if len(item) < 4:
             continue
-        userid, itemid, rating = item[0], item[1], float(item[2])
-        if itemid not in record_dict:
-            record_dict[itemid] = [0, 0]
-        record_dict[itemid][0] += 1
-        record_dict[itemid][1] += rating
+        user_id, item_id, rating = item[0], item[1], float(item[2])
+        if item_id not in record_dict:
+            record_dict[item_id] = [0, 0]
+        record_dict[item_id][0] += 1
+        record_dict[item_id][1] += rating
     fp.close()
-    for itemid in record_dict:
-        score_dict[itemid] = round(record_dict[itemid][1]/record_dict[itemid][0], 3)
+    for item_id in record_dict:
+        score_dict[item_id] = round(record_dict[item_id][1] / record_dict[item_id][0], 3)
     return score_dict
 
-def get_train_data(input_file):
+
+def get_train_data(input_file: str) -> List[int]:
     """
         训练LFM模型
     Args:
-        输入文件
+        input_file:输入文件
     Return:
         链表
     """
@@ -85,34 +87,49 @@ def get_train_data(input_file):
     neg_dict = {}
     pos_dict = {}
     train_data = []
-    linenum = 0
+    line_num = 0
     score_thr = 4.0
     fp = open(input_file)
     for line in fp:
-        if linenum == 0:
-            linenum += 1
+        if line_num == 0:
+            line_num += 1
             continue
         item = line.strip().split(',')
         if len(item) < 4:
             continue
-        userid, itemid, rating = item[0], item[1], float(item[2])
-        if userid not in pos_dict:
-            pos_dict[userid] = []
-        if userid not in neg_dict:
-            neg_dict[userid] = []
+        user_id, item_id, rating = item[0], item[1], float(item[2])
+        if user_id not in pos_dict:
+            pos_dict[user_id] = []
+        if user_id not in neg_dict:
+            neg_dict[user_id] = []
         if rating >= score_thr:
-            pos_dict[userid].append((itemid, 1))
+            pos_dict[user_id].append((item_id, 1))
         else:
-            score = score_dict.get(itemid, 0)
-            neg_dict[userid].append((itemid, score))
+            score = score_dict.get(item_id, 0)
+            neg_dict[user_id].append((item_id, score))
     fp.close()
-    for userid in pos_dict:
-        data_num = min(len(pos_dict[userid]), len(neg_dict.get(userid, [])))
+    for user_id in pos_dict:
+        data_num = min(len(pos_dict[user_id]), len(neg_dict.get(user_id, [])))
         if data_num > 0:
-            train_data += [(userid, zuhe[0], zuhe[1]) for zuhe in pos_dict[userid]][:data_num]
+            train_data += [(user_id, zuhe[0], zuhe[1]) for zuhe in pos_dict[user_id]][:data_num]
         else:
             continue
-        sorted_neg_list = sorted(neg_dict[userid], key=lambda element:element[1], reverse=True)[:data_num]
-        train_data += [(userid, zuhe[0], 0) for zuhe in sorted_neg_list]
+        sorted_neg_list = sorted(neg_dict[user_id], key=lambda element: element[1], reverse=True)[:data_num]
+        train_data += [(user_id, zuhe[0], 0) for zuhe in sorted_neg_list]
     return train_data
 
+
+if __name__ == '__main__':
+    user_file_path = "../data/ratings.txt"
+    item_file_path = "../data/movies.txt"
+    item_info = get_item_info(item_file_path)
+    score_info = get_train_data(user_file_path)
+    # print(item_info)
+    # print(score_info)
+    # s = json.dumps(score_info)
+    # with open('user_item.txt', 'w') as f:
+    #     f.write(s)
+    # with open('user_item.txt', 'r') as f:
+    #     line = f.readline()
+    #     o = json.loads(line)
+    # print(o)
